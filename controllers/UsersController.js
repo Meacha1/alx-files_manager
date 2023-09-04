c4fb012d6e5efcb2bd03c15e5ff598363a1fd047
Meacha1 <meachattd@gmail.com>
2023-09-04 13:23:31 +0300

    user tested

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -26,31 +26,37 @@ class UsersController {
     console.log(`Hashed password: ${hashedPassword}`);
 
     try {
-        // Check if the user already exists
-        const usersCollection = dbClient.db.collection('users');
-        const user = await usersCollection.findOne({ email });
-        if (user) {
-            console.log(`User already exists with email ${email}`);
-            return res.status(400).json({ error: 'Already exist' });
-        }
-    
-        // Create the user
-        const result = await usersCollection.insertOne({
-            email,
-            password: hashedPassword,
-        });
+      // Check if the user already exists
+      const usersCollection = dbClient.db.collection('users');
+      const user = await usersCollection.findOne({ email });
+      if (user) {
+        console.log(`User already exists with email ${email}`);
+        return res.status(400).json({ error: 'Already exist' });
+      }
+
+      // Create the user
+      const result = await usersCollection.insertOne({
+        email,
+        password: hashedPassword,
+      });
+
+      if (result && result.ops && result.ops.length > 0) {
         console.log('User created');
         console.log(result);
-    
-        // Return the new user
-        const newUser = result.ops[0];
+
+        // Return the new user (excluding the password)
+        const newUser = { ...result.ops[0] };
         delete newUser.password;
+
         return res.status(201).json(newUser);
-        }
-        catch (error) {
-        console.log(error);
-        return res.status(500).json({ error: error.message });
-        }
+      } else {
+        console.log('User creation failed');
+        return res.status(500).json({ error: 'User creation failed' });
+      }
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: error.message });
+    }
   }
 }
 
