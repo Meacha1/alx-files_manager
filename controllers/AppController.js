18d90f6ed5515407d6505d0b2258ff79be1912ee
Meacha1 <meachattd@gmail.com>
2023-09-04 11:04:25 +0300

    server

---
+++ controllers/AppController.js
@@ -0,0 +1,24 @@
+const dbClient = require('../utils/db');
+const redisClient = require('../utils/redis');
+
+class AppController {
+  static async getStatus(req, res) {
+    const status = {
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    };
+    return res.status(200).json(status);
+  }
+
+  static async getStats(req, res) {
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+    const stats = {
+      users,
+      files,
+    };
+    return res.status(200).json(stats);
+  }
+}
+
+module.exports = AppController;
---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Load routes from the routes directory
+const routes = require('./routes');
+app.use('/', routes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
