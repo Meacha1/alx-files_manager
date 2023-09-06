9860c706aa2b3313ecdc562821daac6389c3f30f
Meacha1 <meachattd@gmail.com>
2023-09-06 20:37:17 +0300

    post

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -16,80 +16,72 @@ class UpdatedFilesController {
    */
   static async postUpload(request, response) {
     const fileQueue = new Queue('fileQueue');
-    // Retrieve the user based on the token
     const userId = await findUserIdByToken(request);
-    if (!userId) return response.status(401).json({ error: 'Unauthorized' });
 
-    let fileInserted;
-
-    // Validate the request data
-    const { name } = request.body;
-    if (!name) return response.status(400).json({ error: 'Missing name' });
-    const { type } = request.body;
-    if (!type || !['folder', 'file', 'image'].includes(type)) { return response.status(400).json({ error: 'Missing type' }); }
-
-    const isPublic = request.body.isPublic || false;
-    const parentId = request.body.parentId || 0;
-    const { data } = request.body;
-    if (!data && !['folder'].includes(type)) { return response.status(400).json({ error: 'Missing data' }); }
-    // parentId (optional) as ID of the parent (default 0-> root)
-    if (parentId !== 0) {
-      const parentFileArray = await dbClient.files.find({ _id: ObjectID(parentId) }).toArray();
-      if (parentFileArray.length === 0) return response.status(400).json({ error: 'Parent not found' });
-      const file = parentFileArray[0];
-      if (file.type !== 'folder') return response.status(400).json({ error: 'Parent is not a folder' });
+    if (!userId) {
+      return response.status(401).json({ error: 'Unauthorized' });
     }
 
-    // if no data, and not a folder, error
-    if (!data && type !== 'folder') return response.status(400).json({ error: 'Missing Data' });
+    const { name, type, parentId, isPublic, data } = request.body;
 
-    // if type is folder then insert into DB, owner is ObjectID(userId)
-    if (type === 'folder') {
-      fileInserted = await dbClient.files.insertOne({
-        userId: ObjectID(userId),
-        name,
-        type,
-        isPublic,
-        parentId: parentId === 0 ? parentId : ObjectID(parentId),
-      });
-    // if not folder, store file in DB unscrambled
-    } else {
-      // Create a folder for this file
-      const folderPath = process.env.FOLDER_PATH || '/tmp/files_manager';
-      if (!fs.existsSync(folderPath)) fs.mkdirSync(folderPath, { recursive: true }, () => {});
-      // the actual location is the root computer 'C:/tmp/files_manager
+    if (!name) {
+      return response.status(400).json({ error: 'Missing name' });
+    }
 
-      // Create an ID and a new path to the new file
-      const filenameUUID = uuidv4();
-      const localPath = `${folderPath}/${filenameUUID}`;
+    if (!type || !['folder', 'file', 'image'].includes(type)) {
+      return response.status(400).json({ error: 'Missing or invalid type' });
+    }
 
-      // Unscramble data and write to new path
-      const clearData = Buffer.from(data, 'base64');
-      await fs.promises.writeFile(localPath, clearData.toString(), { flag: 'w+' });
-      await fs.readdirSync('/').forEach((file) => {
-        console.log(file);
+    if (type !== 'folder' && !data) {
+      return response.status(400).json({ error: 'Missing data' });
+    }
+
+    let parentFile;
+
+    if (parentId !== 0) {
+      parentFile = await dbClient.files.findOne({
+        _id: ObjectID(parentId),
+        type: 'folder',
       });
 
-      // Insert into the DB
-      fileInserted = await dbClient.files.insertOne({
-        userId: ObjectID(userId),
-        name,
-        type,
-        isPublic,
-        parentId: parentId === 0 ? parentId : ObjectID(parentId),
-        localPath,
-      });
-
-      // if the file is an image, save it in binary
-      if (type === 'image') {
-        await fs.promises.writeFile(localPath, clearData, { flag: 'w+', encoding: 'binary' });
-        await fileQueue.add({ userId, fileId: fileInserted.insertedId, localPath });
+      if (!parentFile) {
+        return response.status(400).json({ error: 'Parent not found or not a folder' });
       }
     }
 
-    // Return the new file with a status code 201
+    const folderPath = process.env.FOLDER_PATH || '/tmp/files_manager';
+    const filenameUUID = uuidv4();
+    const localPath = `${folderPath}/${filenameUUID}`;
+
+    if (type !== 'folder') {
+      const clearData = Buffer.from(data, 'base64');
+      await fs.promises.writeFile(localPath, clearData);
+
+      if (type === 'image') {
+        // Handle image-specific functionality here, e.g., generating thumbnails
+        await fs.promises.writeFile(localPath, clearData, { encoding: 'binary' });
+        await fileQueue.add({ userId, fileId: filenameUUID, localPath });
+      }
+    }
+
+    const fileToInsert = {
+      userId: ObjectID(userId),
+      name,
+      type,
+      isPublic: isPublic || false,
+      parentId: parentId === 0 ? 0 : ObjectID(parentId),
+      localPath: type !== 'folder' ? localPath : null,
+    };
+
+    const result = await dbClient.files.insertOne(fileToInsert);
+
     return response.status(201).json({
-      id: fileInserted.ops[0]._id, userId, name, type, isPublic, parentId,
+      id: result.ops[0]._id.toString(),
+      userId,
+      name,
+      type,
+      isPublic: result.ops[0].isPublic,
+      parentId: result.ops[0].parentId.toString(),
     });
   }
 
